a9af8704ebb6a44ef1f430628b4ee9c9e4e80696
gumdrop <davidcgood@gmail.com>
2017-03-07 13:05:47 +0000

    added notes to report

--- js/src/main/scala/quizleague/web/maintain/results/ResultsComponent.scala
+++ js/src/main/scala/quizleague/web/maintain/results/ResultsComponent.scala
@@ -24,14 +24,21 @@ import quizleague.web.util.Logging
        <div fxLayout="column">
         <h4>Result List</h4>
          <div fxLayout="column">
-          <div *ngFor="let result of item.results;let i = index" fxLayout="row">
-            <button md-icon-button type="button" (click)="removeResult(result)" ><md-icon class="md-24">delete</md-icon></button>
-              <md-input-container>
-                <input mdInput placeholder="{{result.fixture.home.name}}" [(ngModel)]="result.homeScore" name="home{{i}}" type="number">
-              </md-input-container>
-              <md-input-container>
-                <input mdInput placeholder="{{result.fixture.away.name}}" [(ngModel)]="result.awayScore" name="away{{i}}" type="number">
-              </md-input-container>               
+          <div *ngFor="let result of item.results;let i = index" fxLayout="column">
+              <div fxLayout="row">
+                <button md-icon-button type="button" (click)="removeResult(result)" ><md-icon class="md-24">delete</md-icon></button>
+                <md-input-container>
+                  <input mdInput placeholder="{{result.fixture.home.name}}" [(ngModel)]="result.homeScore" name="home{{i}}" type="number">
+                </md-input-container>
+                <md-input-container>
+                  <input mdInput placeholder="{{result.fixture.away.name}}" [(ngModel)]="result.awayScore" name="away{{i}}" type="number">
+                </md-input-container>
+                <button md-icon-button type="button" (click)="toggleNote(result)" ><md-icon class="md-24">note</md-icon></button>
+                <button md-icon-button type="button" (click)="showReports(result)" ><md-icon class="md-24">report</md-icon></button>
+              </div>
+              <md-input-container *ngIf="showNote(result)" >
+                <textarea mdInput placeholder="Notes" [(ngModel)]="result.note" name="note{{i}}" ></textarea>
+              </md-input-container>              
           </div>
          </div>
         </div>      
@@ -51,9 +58,19 @@ class ResultsComponent(
     val router:Router)
     extends ItemComponent[Results] with Logging{
   
+  
+    val showNotes = js.Array[Result]()    
+  
+  
     override def cancel():Unit = location.back()
     
+    
+    
     def removeResult(result:Result) = item.results -= result
+    
+    def toggleNote(result:Result) = if(showNotes.contains(result)) showNotes -= result else showNotes += result
+     
+    def showNote(result:Result) = showNotes.contains(result)
     
     override def save():Unit = {
       service.cache(item)
