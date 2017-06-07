f0c3db684444c67145729d8bdd3c76c76278d568
gumdrop <davidcgood@gmail.com>
2017-06-07 13:16:09 +0100

    reworked etag support

--- jvm/.worksheet/src/temp.scala
+++
@@ -1,3 +0,0 @@
-object temp {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(77); 
-  println(javax.ws.rs.core.EntityTag.valueOf("hello").getValue)}
-}
