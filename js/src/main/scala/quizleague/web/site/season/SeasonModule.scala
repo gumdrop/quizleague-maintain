db9d86c8fdf3bf49a2edb60e8ac7fcbf0a43dc81
gumdrop <davidcgood@gmail.com>
2017-06-26 14:00:18 +0100

    more bits

--- js/src/main/scala/quizleague/web/site/season/SeasonModule.scala
+++ js/src/main/scala/quizleague/web/site/season/SeasonModule.scala
@@ -36,12 +36,9 @@ class SeasonService(override val http: Http,
     override val competitionService: CompetitionService,
     override val venueService: VenueService) extends SeasonGetService with ServiceRoot {
   
-    def getResults(season:Season):Observable[js.Array[Results]] = zip(season.competitions).switchMap((c,i) => 
-        resWithFixs(c.flatMap(_.results))
-        .map((rs,i) => rs.sort(sortPair _)
-            .map(_._1)))
+    def getResults(season:Season):Observable[js.Array[Results]] = zip(season.competitions).switchMap((c,i) => filterAndSort[Results,Fixtures](c.flatMap(_.results),((r:Results) => r.fixtures),(x:Results,y:Fixtures) => true, (r1:(Results,Fixtures),r2:(Results,Fixtures))=> r1._2.date compareTo r2._2.date))
     
-    def getFixtures(season:Season) = zip(season.competitions).map((c,i) => c.flatMap(_.fixtures).sort((f1:Fixtures,f2:Fixtures) => f1.date compareTo f2.date))
+    def getFixtures(season:Season) = zip(season.competitions).switchMap((c,i) => filterAndSort[Fixtures,Fixtures](c.flatMap(_.fixtures),((r:Fixtures) => r),(x:Fixtures,y:Fixtures) => true, (r1:(Fixtures,Fixtures),r2:(Fixtures,Fixtures))=> r1._2.date compareTo r2._2.date))
     
     def getLeagueCompetition(season:Season) = zip(season.competitions).map((cs,i) => cs.filter(_.typeName == "league").head)
 
--- js/src/main/scala/quizleague/web/util/rx/package.scala
+++ js/src/main/scala/quizleague/web/util/rx/package.scala
@@ -5,12 +5,13 @@ package object rx {
   import scala.language.implicitConversions
   import scala.scalajs.js
   import rxjs.Observable
+  import scalajs.js.JSConverters._
   
   implicit def refObsToObs[T](refObs:RefObservable[T]) = refObs.obs
   
   implicit def zip[A](list:js.Array[RefObservable[A]]) = Observable.zip(list.map(_.obs):_*)
   
-  def filterAndSort[T,U](
+  def filterAndSort1[T,U](
       list:js.Array[T], 
       extract:(T => RefObservable[U]), 
       filter:((T,U) => Boolean), 
@@ -22,6 +23,21 @@ package object rx {
         .sort((x:(T,U),y:(T,U)) => sort(x, y)).map(_._1))
     
      interim  
+
+  }
+  
+    def filterAndSort[T,U](
+      list:js.Array[RefObservable[T]], 
+      extract:(T => RefObservable[U]), 
+      filter:((T,U) => Boolean), 
+      sort:(((T,U), (T,U)) => Int)):Observable[js.Array[T]] = {
+      
+    val interim = zip(list).switchMap((l,i) => Observable.zip(l.map(t => extract(t).obs.map((u,i) => (t,u))):_*))
+    .map((tu,i) => tu
+        .filter(x => filter(x._1, x._2))
+        .sort((x:(T,U),y:(T,U)) => sort(x, y)).map(_._1))
+    
+     interim
 
   }
 }
