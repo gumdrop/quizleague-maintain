1446874008b394a3430428f5cfa775c38d15d6db
gumdrop <davidcgood@gmail.com>
2017-06-26 14:01:02 +0100

    more bits

--- js/src/main/scala/quizleague/web/site/season/SeasonModule.scala
+++ js/src/main/scala/quizleague/web/site/season/SeasonModule.scala
@@ -36,9 +36,9 @@ class SeasonService(override val http: Http,
     override val competitionService: CompetitionService,
     override val venueService: VenueService) extends SeasonGetService with ServiceRoot {
   
-    def getResults(season:Season):Observable[js.Array[Results]] = zip(season.competitions).switchMap((c,i) => filterAndSort[Results,Fixtures](c.flatMap(_.results),((r:Results) => r.fixtures),(x:Results,y:Fixtures) => true, (r1:(Results,Fixtures),r2:(Results,Fixtures))=> r1._2.date compareTo r2._2.date))
+    def getResults(season:Season):Observable[js.Array[Results]] = zip(season.competitions).switchMap((c,i) => filterAndSort[Results,Fixtures](c.flatMap(_.results),_.fixtures,(x,y) => true, (r1,r2) => r1._2.date compareTo r2._2.date))
     
-    def getFixtures(season:Season) = zip(season.competitions).switchMap((c,i) => filterAndSort[Fixtures,Fixtures](c.flatMap(_.fixtures),((r:Fixtures) => r),(x:Fixtures,y:Fixtures) => true, (r1:(Fixtures,Fixtures),r2:(Fixtures,Fixtures))=> r1._2.date compareTo r2._2.date))
+    def getFixtures(season:Season) = zip(season.competitions).switchMap((c,i) => filterAndSort[Fixtures,Fixtures](c.flatMap(_.fixtures),r => r,(x:Fixtures,y:Fixtures) => true, (r1:(Fixtures,Fixtures),r2:(Fixtures,Fixtures))=> r1._2.date compareTo r2._2.date))
     
     def getLeagueCompetition(season:Season) = zip(season.competitions).map((cs,i) => cs.filter(_.typeName == "league").head)
 
