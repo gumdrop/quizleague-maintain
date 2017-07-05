b7a221371e38109a99c6e1c0c9434b8c911e8fe4
gumdrop <davidcgood@gmail.com>
2017-07-05 22:26:19 +0100

    removed cache logging

--- js/src/main/scala/quizleague/web/service/CachingService.scala
+++ js/src/main/scala/quizleague/web/service/CachingService.scala
@@ -5,5 +5,5 @@ import quizleague.web.util.Logging._
 trait CachingService[T] {
    this: GetService[T] =>
    
-   override protected[service] def postProcess(domain: U): U = {items += ((domain.id, domain));log(items, s"${domain.getClass.getSimpleName} cache");domain}
+   override protected[service] def postProcess(domain: U): U = {items += ((domain.id, domain));domain}
 }
