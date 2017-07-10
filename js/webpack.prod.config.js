422f72cd6d18cd46848ee70dc50702d599fb8226
gumdrop <davidcgood@gmail.com>
2017-07-10 09:24:01 +0100

    fixed full-opt bundle

--- js/webpack.prod.config.js
+++ js/webpack.prod.config.js
@@ -1,4 +1,4 @@
-angulate2ScalaJsBundler = require("angulate2-scalajs-bundler");
+angulate2ScalaJsBundler = require("./full-opt-jsbundler");
 module.exports = require('./scalajs.webpack.config');
 module.exports.context = __dirname;
 angulate2ScalaJsBundler.apply("quizleague-js", module);
