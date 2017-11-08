bbd1fbe0679b5eb8e288828a209cb23b360b58e9
gumdrop <davidcgood@gmail.com>
2017-11-08 13:43:38 +0000

    ongoing

--- jvm/src/main/scala/quizleague/data/Firebase.scala
+++ jvm/src/main/scala/quizleague/data/Firebase.scala
@@ -11,17 +11,20 @@ import java.util.ArrayList
 import java.util.Collections
 import java.util.Date
 import com.google.cloud.firestore.FirestoreOptions
+import com.google.auth.Credentials
 
 object Firebase {
-  val serviceAccount = this.getClass.getResourceAsStream("/quizleague/auth/firebase.json")
+  val serviceAccount = GoogleCredentials.fromStream(this.getClass.getResourceAsStream("/quizleague/auth/firebase.json"))
 
-  val options = FirestoreOptions.getDefaultInstance.toBuilder().setProjectId("dummy project id").build
+  val options = FirestoreOptions.getDefaultInstance.toBuilder()
+  .setCredentials(serviceAccount)
+  .setProjectId("dummy project id"). build
 
 
   val datastore = options.getService
 
   
-  def save[T <: Entity](entity: T)(implicit tag: ClassTag[T], encoder: Encoder[T]): Key = {
+  def save[T <: Entity](entity: T)(implicit tag: ClassTag[T], encoder: Encoder[T]): Unit = {
     val kind = makeKind
     save(kind, entity.id, encoder(entity))
   }
@@ -29,22 +32,22 @@ object Firebase {
   def load[T <: Entity](id: String)(implicit tag: ClassTag[T], decoder: Decoder[T]): T = load(makeKind, id, decoder)
 
   def list[T <: Entity](implicit tag: ClassTag[T], decoder: Decoder[T]): List[T] = {
-    val q: Query = new Query(makeKind)
-
-    datastore.prepare(q).asIterable().map(entityToObj(_, decoder)).toList
+//    val q: Query = new Query(makeKind)
+//
+//    datastore.prepare(q).asIterable().map(entityToObj(_, decoder)).toList
   }
 
   private def makeKind(implicit tag: ClassTag[_]) = tag.runtimeClass.getSimpleName.toLowerCase
 
-  private def save(kind: String, id: String, json: Json): Key = {
+  private def save(kind: String, id: String, json: Json): Unit = {
 
-    def handleField(entity: PropertyContainer)(name: String, json: Json) = { entity.setProperty(name, convertObject(json)) }
+    def handleField(name: String, json: Json) =  ((name, convertObject(json)))
 
     def convertObject(json: Json): Any = {
       val value: Option[Any] = if (json.isNumber) json.asNumber.map(_.toDouble)
-      else if (json.isString) json.asString.map(_.toString()).map(s => if (s.length() > 1500) new Text(s) else s)
+      else if (json.isString) json.asString.map(_.toString())
       else if (json.isBoolean) json.asBoolean
-      else if (json.isObject) json.asObject.map(doit(new EmbeddedEntity()))
+      else if (json.isObject) json.asObject.map(doit(Map()))
       else if (json.isArray) json.asArray.map(v => asArrayList(v.map(o => convertObject(o))))
       else if (json.isNull) None
       else None
@@ -52,25 +55,20 @@ object Firebase {
       value.getOrElse(null)
     }
 
-    def asArrayList[T](v: Vector[T]): ArrayList[T] = if (v.isEmpty) nullList[T] else new ArrayList(v.toList.asJava)
+    def asArrayList[T](v: Vector[T]): ArrayList[T] = v.toList.asJava
 
-    def doit[T <: PropertyContainer](entity: T)(obj: JsonObject): T = {
+    def doit(entity: Map[String,Any])(obj: JsonObject) = {
 
-      obj.toVector.foreach((handleField(entity) _).tupled)
-
-      entity
+      entity ++ obj.toVector.map((handleField _).tupled)
     }
 
-    val t = datastore.beginTransaction()
+   // val t = datastore.beginTransaction()
 
-    val key = json.asObject.map(obj => datastore.put(t, doit(new Ent(kind, id))(obj)))
+    val key = json.asObject.map(obj => datastore.document(s"$kind/$id").create(doit(Map())(obj)))
 
-    t.commit()
-
-    key.get
   }
 
-  private def props(p: PropertyContainer) = p.getProperties.asScala.toList.map({ case (name: String, o) => (name, convertToJson(o)) })
+  private def props(p: java.util.Map[String,Any]) = p.asScala.toList.map({ case (name: String, o) => (name, convertToJson(o)) }).asJava
 
   private def convertToJson(obj: Any): Json = {
 
@@ -78,9 +76,8 @@ object Firebase {
       case null => Json.Null
       case d: java.lang.Double => Json.fromDoubleOrNull(d)
       case s: String => Json.fromString(s)
-      case t: Text => Json.fromString(t.getValue)
       case b: Boolean => Json.fromBoolean(b)
-      case ee: EmbeddedEntity => Json.fromFields(props(ee))
+      case ee: java.util.Map[String,Any] => Json.fromFields(props(ee))
       case l: java.util.List[_] => Json.fromValues(l.filter(_ != null).map(convertToJson(_)))
       case _ => throw new RuntimeException(s"no match found for ${obj.getClass.getName}")
     }
