c5d04d24b276d9d1c7e76349639cefd6dbd4408c
gumdrop <davidcgood@gmail.com>
2018-02-08 19:36:23 +0000

    removed spurious ignore

---
+++ shared/src/main/scala/quizleague/StringUtils.scala
@@ -0,0 +1,13 @@
+package quizleague.util
+
+import scala.language.implicitConversions
+
+object StringUtils {
+     implicit class StringImprovements(val s: String) {
+         import scala.util.control.Exception._
+         def toIntOpt = catching(classOf[NumberFormatException]) opt s.toInt
+         def toLongOpt = catching(classOf[NumberFormatException]) opt s.toLong
+     }
+     def toOrdinal(n:Int)=n+{if(n%100/10==1)"th"else(("thstndrd"+"th"*6).sliding(2,2).toSeq(n%10))}
+}
+
