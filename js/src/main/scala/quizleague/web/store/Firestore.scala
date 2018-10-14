5f77df5db9904a87611bd0e2064dadcacd53b513
gumdrop <davidcgood@gmail.com>
2018-10-14 13:00:09 +0100

    reset refs

--- js/src/main/scala/quizleague/web/store/Firestore.scala
+++ js/src/main/scala/quizleague/web/store/Firestore.scala
@@ -7,22 +7,22 @@ import scala.scalajs.js.JSConverters._
 
 
 object Firestore {
-//    val config = literal(      
-//    apiKey= "AIzaSyBs6LpcOSpLMlKlzw0aPB6Ie-39mqlKrm8",
-//    authDomain= "chiltern-ql-firestore.firebaseapp.com",
-//    databaseURL= "https://chiltern-ql-firestore.firebaseio.com",
-//    projectId= "chiltern-ql-firestore",
-//    storageBucket= "chiltern-ql-firestore.appspot.com",
-//    messagingSenderId= "891716942638"
-//)
-
     val config = literal(      
-    apiKey= "AIzaSyCTnCW1euWGpRohoEBESIdNEASM7rQ5gkY",
-    authDomain= "ql-firestore-2.firebaseapp.com",
-    databaseURL= "https=//ql-firestore-2.firebaseio.com",
-    projectId= "ql-firestore-2",
-    storageBucket= "ql-firestore-2.appspot.com",
-    messagingSenderId= "659931577179")
+    apiKey= "AIzaSyBs6LpcOSpLMlKlzw0aPB6Ie-39mqlKrm8",
+    authDomain= "chiltern-ql-firestore.firebaseapp.com",
+    databaseURL= "https://chiltern-ql-firestore.firebaseio.com",
+    projectId= "chiltern-ql-firestore",
+    storageBucket= "chiltern-ql-firestore.appspot.com",
+    messagingSenderId= "891716942638"
+)
+
+//    val config = literal(      
+//    apiKey= "AIzaSyCTnCW1euWGpRohoEBESIdNEASM7rQ5gkY",
+//    authDomain= "ql-firestore-2.firebaseapp.com",
+//    databaseURL= "https=//ql-firestore-2.firebaseio.com",
+//    projectId= "ql-firestore-2",
+//    storageBucket= "ql-firestore-2.appspot.com",
+//    messagingSenderId= "659931577179")
 
   
    Firebase.initializeApp(config)
