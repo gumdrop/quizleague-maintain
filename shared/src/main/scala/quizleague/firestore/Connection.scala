ef7f583d8655f77e2646f2939c525a3f8e003f4b
gumdrop <davidcgood@gmail.com>
2019-05-24 13:32:37 +0100

    single source connection

---
+++ shared/src/main/scala/quizleague/firestore/Connection.scala
@@ -0,0 +1,10 @@
+package quizleague.firestore
+
+object Connection{
+  val apiKey = "AIzaSyCTnCW1euWGpRohoEBESIdNEASM7rQ5gkY"
+  val authDomain = "ql-firestore-2.firebaseapp.com"
+  val databaseURL = "https=//ql-firestore-2.firebaseio.com"
+  val projectId = "ql-firestore-2"
+  val storageBucket  = "ql-firestore-2.appspot.com"
+  val messagingSenderId = "659931577179"
+}
