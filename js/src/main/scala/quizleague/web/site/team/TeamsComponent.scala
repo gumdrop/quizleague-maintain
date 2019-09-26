9db8ce55de62e11da7bc1b1e53fccd88024d005b
gumdrop <davidcgood@gmail.com>
2019-09-26 10:33:08 +0100

    fixed start team

--- js/src/main/scala/quizleague/web/site/team/TeamsComponent.scala
+++ js/src/main/scala/quizleague/web/site/team/TeamsComponent.scala
@@ -39,7 +39,7 @@ object StartTeamPage extends RouteComponent with GridSizeComponentConfig{
     val template="""
       <v-container v-bind="gridSize" fluid>
         <v-layout>
-          <v-flex><ql-named-text name="start-team"></ql-named-text></v-flex>
+          <v-flex><ql-text-box><ql-named-text name="start-team"></ql-named-text></ql-text-box></v-flex>
         </v-layout>
        </v-container>"""
 
@@ -48,11 +48,12 @@ object StartTeamPage extends RouteComponent with GridSizeComponentConfig{
 object StartTeamTitleComponent extends RouteComponent{
    val  template="""
     <v-toolbar      
-      color="amber darken-3"
-      dark
+      color="amber lighten-3"
+      dense
+      class="subtitle-background"
       >
       <ql-title>Starting a Team</ql-title>
-      <v-toolbar-title class="white--text" >
+      <v-toolbar-title>
         Starting a Team 
       </v-toolbar-title>
     </v-toolbar>"""
