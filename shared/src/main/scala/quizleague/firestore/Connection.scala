b36b02b622da65c753f74dfbe8eb6053d8f68671
gumdrop <davidcgood@gmail.com>
2020-06-24 15:49:20 +0100

    switched back to prod

--- shared/src/main/scala/quizleague/firestore/Connection.scala
+++ shared/src/main/scala/quizleague/firestore/Connection.scala
@@ -1,13 +1,13 @@
 package quizleague.firestore
 
-//object Connection{
-//  val apiKey = "AIzaSyBs6LpcOSpLMlKlzw0aPB6Ie-39mqlKrm8"
-//  val authDomain = "chiltern-ql-firestore.firebaseapp.com"
-//  val databaseURL = "https://chiltern-ql-firestore.firebaseio.com"
-//  val projectId = "chiltern-ql-firestore"
-//  val storageBucket  = "chiltern-ql-firestore.appspot.com"
-//  val messagingSenderId = "891716942638"
-//}
+object Connection{
+  val apiKey = "AIzaSyBs6LpcOSpLMlKlzw0aPB6Ie-39mqlKrm8"
+  val authDomain = "chiltern-ql-firestore.firebaseapp.com"
+  val databaseURL = "https://chiltern-ql-firestore.firebaseio.com"
+  val projectId = "chiltern-ql-firestore"
+  val storageBucket  = "chiltern-ql-firestore.appspot.com"
+  val messagingSenderId = "891716942638"
+}
 
 //object Connection{
 //  val apiKey = "AIzaSyCTnCW1euWGpRohoEBESIdNEASM7rQ5gkY"
@@ -18,15 +18,15 @@ package quizleague.firestore
 //  val messagingSenderId = "659931577179"
 //}
 
-object Connection {
-  val apiKey = "AIzaSyCszf1qcAadBzISfaQPHvwB2qGyVGJU08U"
-  val authDomain = "ql-firestore-gb2.firebaseapp.com"
-  val databaseURL= "https=//ql-firestore-gb2.firebaseio.com"
-  val projectId= "ql-firestore-gb2"
-  val storageBucket= "ql-firestore-gb2.appspot.com"
-  val messagingSenderId= "415442183710"
-  val appId= "1=415442183710=web=ae2f7f4f50a2c56d72700f"
-}
+//object Connection {
+//  val apiKey = "AIzaSyCszf1qcAadBzISfaQPHvwB2qGyVGJU08U"
+//  val authDomain = "ql-firestore-gb2.firebaseapp.com"
+//  val databaseURL= "https=//ql-firestore-gb2.firebaseio.com"
+//  val projectId= "ql-firestore-gb2"
+//  val storageBucket= "ql-firestore-gb2.appspot.com"
+//  val messagingSenderId= "415442183710"
+//  val appId= "1=415442183710=web=ae2f7f4f50a2c56d72700f"
+//}
 
 
 
